0928ab65168c6c944db0045531bd2b0c0ab7d3a1
BlackJack255 <noonejack255@gmail.com>
2026-08-18 10:40:39 +0800

    [feat] turn car door text to bold when revealing final doors

--- 002_Three_Doors_Problem/script.js
+++ 002_Three_Doors_Problem/script.js
@@ -231,12 +231,15 @@ function reveal(event){
     let result_str = `The door you chose is ${picked+1}, thing behind is ... a`
     if (door_arr[picked] == CAR){
         doorObj_Arr[picked].style.color = 'green'
+        doorObj_Arr[picked].style.fontWeight = "bold"
         event.target.style.color = 'green'
+        event.target.style.fontWeight = "bold"
         result_str = result_str + " car, you win!"
     }
     else{
         doorObj_Arr[picked].style.color = 'red'
         doorObj_Arr[car_price_idx].style.color = 'red'
+        doorObj_Arr[car_price_idx].style.fontWeight = "bold"
         event.target.style.color = 'red'
         result_str = result_str + " goat, sorry."
     }
